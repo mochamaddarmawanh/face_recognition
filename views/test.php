8b31463cbb96e9ba75c1cc15775510653983e872
mochamaddarmawanh <mochamaddarmawanh@gmail.com>
2023-07-10 15:43:51 +0700

    test model page view

--- views/test.php
+++ views/test.php
@@ -1,4 +1,99 @@
-<!-- content -->
-<main class="container" style="margin-top: 200px;">
-    Training From Webcam
-</main>
+    <!-- content -->
+    <main class="container" style="margin-top: 200px; margin-bottom: 200px;">
+        <h1 class="fw-bold">Test Model</h1>
+        <div class="col-12 col-lg-5">
+            <div class="card shadow-sm mb-3">
+                <div class="card-header bg-white">
+                    <div class="row">
+                        <div class="col-11">
+                            <h5>Preview</h5>
+                        </div>
+                        <div class="col-1">
+                            <div class="dropdown">
+                                <i class="bi-three-dots-vertical mt-1" type="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li><a class="dropdown-item" style="cursor: pointer;">Webcam</a></li>
+                                    <li><span class="dropdown-item" style="cursor: pointer;">Upload</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+
+
+
+
+                    <p>Select Methods:</p>
+                    <button class="btn btn-sm btn-primary">
+                        <i class="bi-camera-video"></i>
+                        <br>
+                        Webcam
+                    </button>
+                    <button class="btn btn-sm btn-primary">
+                        <i class="bi-upload"></i>
+                        <br>
+                        Upload
+                    </button>
+
+
+
+
+
+
+
+
+
+                    <!-- <div class="row mb-1">
+                        <div class="col-10 mt-1">
+                            <p>File</p>
+                        </div>
+                        <div class="col-2 mt-1">
+                            <div class="text-dark" style="cursor: pointer;">
+                                <i class="bi-x float-end" style="font-size: 35px; margin-top: -10px; margin-right: -5px;"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <input type="file" name="" id="" class="form-control">
+                </div>
+                <div class="card-footer bg-white">
+                    <p>Output:</p>
+                    <img src="assets/img/other/face-api-js.gif" alt="" class="img-fluid">
+                    <p class="mt-3" style="margin-bottom: 10px; font-weight: 600;">1. Mochamad Darmawan Hardjakusumah</p>
+                    <div class="progress mb-3" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-dark" style="width: 25%">25%</div>
+                    </div>
+                    <p class="mt-3" style="margin-bottom: 10px; font-weight: 600;">2. Fahrezi Huda Yusron</p>
+                    <div class="progress mb-3" role="progressbar" aria-label="Example with label" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-dark" style="width: 99%">99%</div>
+                    </div>
+                </div> -->
+
+
+
+
+                <!-- <div class="row mb-1">
+                        <div class="col-10 mt-1">
+                            <p>Webcam</p>
+                        </div>
+                        <div class="col-2 mt-1">
+                            <div class="text-dark" style="cursor: pointer;">
+                                <i class="bi-x float-end" style="font-size: 35px; margin-top: -10px; margin-right: -5px;"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <select name="" id="" class="form-select">
+                        <option value="">Switch Webcam</option>
+                    </select>
+                    <video class="rounded-2 mt-2 w-100" controls></video>
+                </div>
+                <div class="card-footer bg-white">
+                    <p>Output:</p>
+                    <p class="" style="margin-bottom: 10px; font-weight: 600;">Mochamad Darmawan Hardjakusumah</p>
+                    <div class="progress mb-3" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-dark" style="width: 25%">25%</div>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </main>
